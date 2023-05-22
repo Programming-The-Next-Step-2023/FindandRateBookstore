3eeebe08db68f77dda5ebec991365845b9df8c44
rebekka-cebulla <github.rebekka@cebulla.name>
2023-05-22 10:40:39 +0200

    code

---
+++ code1.R
@@ -0,0 +1,36 @@
+install.packages("shiny")
+install.packages("leaflet")
+install.packages("maps")
+
+
+library(leaflet) #for making the interactive map
+library(shiny)
+library(maps)
+
+
+##the user interface 
+
+ui <- fluidPage(
+  titlePanel("Find and Rate a Bookstore"),
+  theme = shinytheme("cerulean"),
+  leafletOutput("map_netherlands")
+  
+)
+
+
+##the server
+
+server <- function(input, output) {
+  output$map_netherlands <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = 5.2913, lat = 52.1326, zoom = 7)
+  })
+}
+
+
+##run the app
+shinyApp(ui, server)
+
+
+
