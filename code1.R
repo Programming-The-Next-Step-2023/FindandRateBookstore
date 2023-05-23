38f012a092243af00196c8f2a9b48828ae9ff017
rebekka-cebulla <github.rebekka@cebulla.name>
2023-05-23 12:06:39 +0200

    update code week 2
    I added a side panel where users can type in their location

--- code1.R
+++ code1.R
@@ -12,25 +12,49 @@ library(maps)
 
 ui <- fluidPage(
   titlePanel("Find and Rate a Bookstore"),
-  theme = shinytheme("cerulean"),
-  leafletOutput("map_netherlands")
-
+  sidebarLayout(
+    position = "right",
+    sidebarPanel(
+      h1("Where are you right now?"),
+      textInput("latitude", "Latitude:"),
+      textInput("longitude", "Longitude:"),
+      actionButton("updateMap", "Update Map")
+      ),
+    mainPanel(
+      h1("map"),
+      leafletOutput("map_netherlands")
+      )
+  )
 )
 
 
-##the server
 
+##the server
+#fist I want there to be a map of the Netherlands when the app opens
 server <- function(input, output) {
   output$map_netherlands <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       setView(lng = 5.2913, lat = 52.1326, zoom = 7)
   })
+#then the user can type in their location using Latitude and Longitude
+  #the map then zooms in closer to their location on the map
+  observeEvent(input$updateMap, {
+    lat <- as.numeric(input$latitude)
+    lng <- as.numeric(input$longitude)
+
+    if (!is.na(lat) && !is.na(lng)) {
+      leafletProxy("map_netherlands") %>%
+        clearMarkers() %>%
+        addMarkers(lng = lng, lat = lat, label = "Your Location") %>%
+        setView(lng = lng, lat = lat, zoom = 14)
+    }
+  })
 }
 
 
 ##run the app
-shinyApp(ui, server)
+shinyApp(ui= ui, server= server)
 
 
 #comment
