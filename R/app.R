f9b4e2ae459f138c928c8bc7bb07b71bc8b10695
rebekka-cebulla <github.rebekka@cebulla.name>
2023-05-26 22:52:56 +0200

    week 3 push
    adding a r-markdown file with the outline for the vignette and also some changes in code

---
+++ R/app.R
@@ -0,0 +1,132 @@
+# install.packages("shiny")
+# install.packages("leaflet")
+# install.packages("maps")
+# #install.packages("Rtools")
+# install.packages("rlang")
+# install.packages("shinytest")
+# install.packages("testthat")
+# install.packages("usethis")
+#install.packages("shinyWidgets")
+
+install.packages("rmarkdown")
+
+library(leaflet) #for making the interactive map
+library(shiny)
+library(maps)
+library(rlang)
+library(usethis)
+library(devtools)
+library(testthat)
+library(shinytest)
+library(FindandRateBookstore)
+library(shinyWidgets)
+
+
+devtools::document()
+devtools::install("FindandRateBookstore")
+
+#' @importFrom usethis use_package_doc
+#' @export
+run_use_package_doc <- function() {
+  usethis::use_package_doc(override = TRUE, includeFiles = TRUE)
+}
+
+
+#add all the things i want to instal to the discription file
+##the user interface
+
+ui <- fluidPage(
+  #set the backgroundcolour of the shiny app page
+  setBackgroundColor(
+    color = "#d9f0c1",
+    gradient = c("linear", "radial"),
+    direction = c("bottom", "top", "right", "left"),
+    shinydashboard = FALSE
+  ),
+  #add a title
+  titlePanel(
+    tags$div(
+    # Use the img tag to add the image
+      tags$img(src = system.file("R/images/Picture5.gif", package = "FindandRateBookstore"),
+               height = "150px", width = "150px"),
+    # Add a space between the image and text
+    tags$span(style = "margin-left: 20px;"),
+    # Add the text
+    "Find and Rate a Bookstore"),
+  ),
+  #add a side bar
+  sidebarLayout(
+    position = "right",
+    #panel on the right that allows for latitude and longitude input
+    sidebarPanel(
+      h1("Where are you right now?"),
+      textInput("latitude", "Latitude:"),
+      helpText("Enter the latitude of your location."),
+      textInput("longitude", "Longitude:"),
+      helpText("Enter the longitude of your location."),
+      actionButton("updateMap", "Update Map")
+      ),
+    #main panel on the left that depicts a map
+    mainPanel(
+      h1("Map"),
+      leafletOutput("map_netherlands"),
+      br(), # Add a line break
+      textOutput("map_description") # Display text below the map
+    )
+
+  )
+)
+
+
+
+###### delete this documentation
+
+##the server
+#fist I want there to be a map of the Netherlands when the app opens
+
+
+#' create a Map
+#'
+#' @param input latitude and longitude of the location
+#' @param output map zoomed into the location
+#'
+#' @return the map
+#' @export
+#'
+#' @examples
+#' if you pot in latitude: 52.37 and longitude: 4.89 then it will zoom into Amsterdam
+
+
+server <- function(input, output) {
+  output$map_netherlands <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = 5.2913, lat = 52.1326, zoom = 7)
+  })
+#then the user can type in their location using Latitude and Longitude
+  #the map then zooms in closer to their location on the map
+  observeEvent(input$updateMap, {
+    lat <- as.numeric(input$latitude)
+    lng <- as.numeric(input$longitude)
+
+    if (!is.na(lat) && !is.na(lng)) {
+      leafletProxy("map_netherlands") %>%
+        clearMarkers() %>%
+        addMarkers(lng = lng, lat = lat, label = "Your Location") %>%
+        setView(lng = lng, lat = lat, zoom = 14)
+    }
+
+  })
+  #add the text for under the map
+    output$map_description <- renderText({
+      "Add you longitude and latitude to the right and this map will show you your location"
+      })
+}
+
+
+#add documentation to this function
+#this is the function that will run the app
+startApp <- function(){
+shinyApp(ui= ui, server= server) }
+
+
---
+++ tests/testthat.R
@@ -0,0 +1,47 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/tests.html
+# * https://testthat.r-lib.org/reference/test_package.html#special-files
+
+install.packages("FindandRateBookstore")
+install.packages("Rtools")
+
+devtools::install_github("Programming-The-Next-Step-2023/FindandRateBookstore")
+
+library(testthat)
+library(FindandRateBookstore)
+library(shinytest)
+library(shiny)
+library(leaflet)
+
+#test_check("FindandRateBookstore")
+
+# Define a test case for map zoom
+
+
+test_that("Map zooms in correctly", {
+  #start a new app session
+  app <- ShinyDriver$new(testthat::test_path("startApp.R"), loadTimeout = 100000)
+
+  # Set the latitude and longitude inputs
+  app$setInputs(latitude = "52.0", longitude = "5.0", wait = FALSE, val = FALSE)
+
+  # Click the "Update Map" button
+  app$click("updateMap")
+
+  # Get the current map state
+  map_state <- app$getMapState("map_netherlands")
+
+
+# Check if the map zoom level is correct
+  expect_equal(map_state$zoom, 14)
+
+  # Close the Shiny app session
+  app$stop()
+})
+
+
+
